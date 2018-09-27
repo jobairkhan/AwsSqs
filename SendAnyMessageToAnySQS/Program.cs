cc84b6ff14d27e3a65ba5d018754c1524358db5c
Jobair Khan <jobair.khan@groupimd.com>
2018-09-27 09:50:07 +0100

    Completed

--- SendAnyMessageToAnySQS/Program.cs
+++ SendAnyMessageToAnySQS/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon.Extensions.NETCore.Setup;
@@ -12,6 +11,13 @@ namespace SendAnyMessageToAnySQS
 {
     public class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">
+        /// 1. Number of Messages to send
+        /// 2. Optional - Confirm or not
+        /// </param>
         public static void Main(string[] args)
         {
             _config = new ConfigurationBuilder()
@@ -31,15 +37,18 @@ namespace SendAnyMessageToAnySQS
                 numberOfMessage = BATCH_SIZE;
             }
 
-            SendToQ(options, numberOfMessage).GetAwaiter().GetResult();
+            bool.TryParse((args.Length > 1 ? args[1] : "false"), out var proceed);
+
+
+            SendToQ(options, numberOfMessage, proceed).GetAwaiter().GetResult();
         }
 
         private static IConfiguration _config;
-        private const string QUEUE_NAME = "webhooks_events";
-        private const int BATCH_SIZE = 1;
+        private const string QUEUE_NAME = "db-webhooks_events";
+        private const int BATCH_SIZE = 10;
         private const string AWS_ACCOUNT_ID = "130075576898";
 
-        internal static async Task SendToQ(AWSOptions options, int numberOfMessage)
+        internal static async Task SendToQ(AWSOptions options, int numberOfMessage, bool confirm)
         {
             var messageCount = 0;
             using (var sqs = options.CreateServiceClient<IAmazonSQS>())
@@ -48,13 +57,21 @@ namespace SendAnyMessageToAnySQS
                 try
                 {
                     var qUrl = await GetQueueUrl(sqs, QUEUE_NAME);
-                    var proceed = Confirmation(numberOfMessage, qUrl);
-
-                    while (proceed && messageCount < numberOfMessage)
+                    if (!confirm)
                     {
-                        await SendBatchMessage(sqs, qUrl, BATCH_SIZE);
+                        confirm = Confirmation(numberOfMessage, qUrl);
+                    }
 
-                        messageCount += BATCH_SIZE;
+
+                    while (confirm && messageCount < numberOfMessage)
+                    {
+                        var requestMsg = Math.Abs(numberOfMessage - messageCount); 
+                        
+                        requestMsg = requestMsg > BATCH_SIZE ? BATCH_SIZE : requestMsg; 
+
+                        await SendBatchMessage(sqs, qUrl, requestMsg);
+
+                        messageCount += requestMsg;
                     }
                 }
                 catch (AmazonSQSException ex)
@@ -67,44 +84,23 @@ namespace SendAnyMessageToAnySQS
                 }
             }
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("\r\nTotal sent {0} messages", messageCount);
             Console.WriteLine("Press Enter to exit...");
             Console.ReadLine();
-        }
-
-        private static bool Confirmation(int numberOfMessage, string qUrl)
-        {
-            Console.WriteLine("===========================================");
-            Console.WriteLine("Preparing to send {0} messages to {1}", numberOfMessage, qUrl);
-            Console.Write("Enter Esc to exit OR Enter any key to proceed! ");
-            var key  = Console.ReadKey();
-            if (key.Key == ConsoleKey.Escape) return false;
-            Console.WriteLine("===========================================\n");
-            return true;
-        }
-
-        private static void ConsoleError(AmazonSQSException ex)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Caught AWS Exception: " + ex.Message);
-            Console.WriteLine("Response Status Code: " + ex.StatusCode);
-            Console.WriteLine("Error Code: " + ex.ErrorCode);
-            Console.WriteLine("Error Type: " + ex.ErrorType);
-            Console.WriteLine("Request ID: " + ex.RequestId);
-            Console.ForegroundColor = ConsoleColor.White;
         }
 
         private static async Task SendBatchMessage(IAmazonSQS amazonSqsClient, string queueUrl, int batchSize)
         {
             var request = new SendMessageBatchRequest
             {
-                Entries = new List<SendMessageBatchRequestEntry>() {  },
+                Entries = new List<SendMessageBatchRequestEntry>() { },
                 QueueUrl = queueUrl
             };
 
-            for (int i = 0; i < batchSize; i++)
+            for (var i = 0; i < batchSize; i++)
             {
-                request.Entries.Add(new SendMessageBatchRequestEntry());
+                request.Entries.Add(CreateNewEntry($"{i}-{batchSize}"));
             }
 
 
@@ -112,20 +108,17 @@ namespace SendAnyMessageToAnySQS
 
             if (response.Successful.Count > 0)
             {
-                Console.WriteLine("Successfully sent:");
+                Console.WriteLine("Successfully sent {0} message", response.Successful.Count);
 
                 foreach (var success in response.Successful)
                 {
                     Console.WriteLine("  For ID: '" + success.Id + "':");
                     Console.WriteLine("    Message ID = " + success.MessageId);
-                    Console.WriteLine("    MD5 of message attributes = " +
-                                      success.MD5OfMessageAttributes);
+                    //Console.WriteLine("    MD5 of message attributes = " +
+                    //                  success.MD5OfMessageAttributes);
                     Console.WriteLine("    MD5 of message body = " +
                                       success.MD5OfMessageBody);
                 }
-
-                //Console.WriteLine("Successfully sent {0} messages with batch id ({1})", response.Successful.Count, _batchId);
-                //_messageCount += response.Successful.Count;
             }
 
             if (response.Failed.Count > 0)
@@ -157,5 +150,43 @@ namespace SendAnyMessageToAnySQS
             Console.WriteLine(createQueueResponse.QueueUrl);
             return createQueueResponse.QueueUrl;
         }
+
+        private static bool Confirmation(int numberOfMessage, string qUrl)
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Preparing to send {0} messages to {1}", numberOfMessage, qUrl);
+            Console.Write("Enter Esc to exit OR Enter any key to proceed! ");
+            var key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("No. User chose to exit process");
+                return false;
+            }
+            Console.WriteLine("Yes");
+            Console.WriteLine("===========================================");
+            return true;
+        }
+
+        private static void ConsoleError(AmazonSQSException ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Caught AWS Exception: " + ex.Message);
+            Console.WriteLine("Response Status Code: " + ex.StatusCode);
+            Console.WriteLine("Error Code: " + ex.ErrorCode);
+            Console.WriteLine("Error Type: " + ex.ErrorType);
+            Console.WriteLine("Request ID: " + ex.RequestId);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static SendMessageBatchRequestEntry CreateNewEntry(string id)
+        {
+            return new SendMessageBatchRequestEntry
+            {
+                DelaySeconds = 0,
+                Id = id,
+                MessageBody = "{\"eventTypeId\":1,\"eventTypeName\":\"Qc\",\"referenceId\":1,\"accounts\":[12],\"body\":{\"fileId\":1,\"versionId\":1,\"statusId\":1,\"statusName\":\"Passed\",\"attributes\":[{\"id\":1,\"name\":\"name\",\"value\":\"a value\"}]}}"
+            };
+        }
+
     }
 }
